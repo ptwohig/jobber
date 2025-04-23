3dac74ee3d6ad171129777074b6c7222924eacc3
Patrick Twohig <patrick@namazustudios.com>
2025-04-23 10:40:42 -0700

    Enhanced the job description summary.

--- src/main/java/com/patricktwohig/jobber/model/JobDescriptionSummary.java
+++ src/main/java/com/patricktwohig/jobber/model/JobDescriptionSummary.java
@@ -13,6 +13,10 @@ public class JobDescriptionSummary {
 
     private String summary;
 
+    private String companyValues;
+
+    private String companyMission;
+
     private List<String> skills;
 
     public String getTitle() {
@@ -55,16 +59,32 @@ public class JobDescriptionSummary {
         this.skills = skills;
     }
 
+    public String getCompanyValues() {
+        return companyValues;
+    }
+
+    public void setCompanyValues(String companyValues) {
+        this.companyValues = companyValues;
+    }
+
+    public String getCompanyMission() {
+        return companyMission;
+    }
+
+    public void setCompanyMission(String companyMission) {
+        this.companyMission = companyMission;
+    }
+
     @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        JobDescriptionSummary that = (JobDescriptionSummary) o;
-        return Objects.equals(getTitle(), that.getTitle()) && Objects.equals(getCompany(), that.getCompany()) && Objects.equals(getLocation(), that.getLocation()) && Objects.equals(getSummary(), that.getSummary()) && Objects.equals(getSkills(), that.getSkills());
+    public boolean equals(Object object) {
+        if (object == null || getClass() != object.getClass()) return false;
+        JobDescriptionSummary that = (JobDescriptionSummary) object;
+        return Objects.equals(getTitle(), that.getTitle()) && Objects.equals(getCompany(), that.getCompany()) && Objects.equals(getLocation(), that.getLocation()) && Objects.equals(getSummary(), that.getSummary()) && Objects.equals(getCompanyValues(), that.getCompanyValues()) && Objects.equals(getCompanyMission(), that.getCompanyMission()) && Objects.equals(getSkills(), that.getSkills());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getTitle(), getCompany(), getLocation(), getSummary(), getSkills());
+        return Objects.hash(getTitle(), getCompany(), getLocation(), getSummary(), getCompanyValues(), getCompanyMission(), getSkills());
     }
 
     @Override
@@ -74,6 +94,8 @@ public class JobDescriptionSummary {
         sb.append(", company='").append(company).append('\'');
         sb.append(", location='").append(location).append('\'');
         sb.append(", summary='").append(summary).append('\'');
+        sb.append(", companyValues='").append(companyValues).append('\'');
+        sb.append(", companyMission='").append(companyMission).append('\'');
         sb.append(", skills=").append(skills);
         sb.append('}');
         return sb.toString();
